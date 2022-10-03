5a2b65e20ba6bbb2dab158334c13cfb48148ee9d
Thomas <thomaskouadio92@gmail.com>
2022-10-04 01:46:57 +0200

    ajout du parseur de la table (2 versions)

--- src/Parser.java
+++ src/Parser.java
@@ -1,34 +1,64 @@
+import static java.lang.System.*;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 
 public class Parser {
 
-    public static void main(String args[]) throws FileNotFoundException 
-   {
+    public static void main(String args[]) throws FileNotFoundException {
  
-        //creating File instance to reference text file in Java
-        File sql = new File("PATH TO .SQL FILE");
-     
-        //Creating Scanner instance to read File in Java
-        Scanner scnr = new Scanner(sql);
-     
-        //Reading each line of the file using Scanner class
+        File jMeriseSQL = new File("./test.sql");
+        Scanner scanner = new Scanner(jMeriseSQL);
+        
         int lineNumber = 1;
-        while(scnr.hasNextLine())
-        {
-            String strCurrentLine = scnr.nextLine();
+        char parenthese = '(';
+        
+        String createTableStr = "CREATE TABLE ";
+        String tableResult;
+        String[] list_of_tables = {};
+        
+        Pattern createTablePattern = Pattern.compile(createTableStr, Pattern.CASE_INSENSITIVE);
+        
+        while(scanner.hasNextLine()) {
+        	
+            String strCurrentLine = scanner.nextLine();
+            int currenLineLength = strCurrentLine.length();
             lineNumber++;
-            
-            if (strCurrentLine.startsWith("CREATE TABLE"))
-            {
-                System.out.println("line " + lineNumber + " :" + strCurrentLine);
+        
+                      
+            if (strCurrentLine.startsWith(createTableStr)) {
+             
+                 tableResult = "";
 
-            }
-            else 
-            	{
+	    		 for (int i = 0; i < currenLineLength; i++) {
+	
+                     // ------------ INDEX VERSION ----------- //
+	    			 char currentCaracter = strCurrentLine.charAt(i);
+	    		        // Si l'on tombe apres le CREATE TABLE et l'espace
+	    		    if (i >= createTableStr.length()) {
+	
+	    		        // ... ET si l'on ne tombe pas sur la parenthese ouvrante
+	    		        if (!(Character.compare(currentCaracter,parenthese) == 0)) {
+	    		                 tableResult += Character.toString(currentCaracter);
+	    		        }
+	    		     }
+	    		 }
+	    		 
+	             // -------- REGEX VERSION ------------ // 
+	             Matcher create_table_matcher = createTablePattern.matcher(strCurrentLine);
+	             
+                if (create_table_matcher.find()) {
+                	tableResult = strCurrentLine.replaceAll(createTableStr, "").replace(Character.toString(parenthese), "").trim();
+                	out.println(tableResult);
+                }
+                
+               System.out.println(tableResult); 
+            } else {
             	strCurrentLine = "";
-            	}
+            }
         }			
     }      
  }  

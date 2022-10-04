4dece2e1090b141d0c1eb5100d87fc934271e8ae
Thomas <thomaskouadio92@gmail.com>
2022-10-04 02:11:47 +0200

    ajout du parseur de la table - refactoring en 2 methodes

--- src/Parser.java
+++ src/Parser.java
@@ -7,58 +7,73 @@ import java.util.regex.Pattern;
 
 
 public class Parser {
+	
+	private static String createTableStr = "CREATE TABLE ";
+	private static String tableResult = "";
+	private static char parenthese = '(';
+	
+	/**
+	 * getTablenameByIndex => get the table name(s) of generated .SQL or text file (works for Jmerise) - Search by string indexes
+	 * @param currentLine
+	 * @return {String} The tablename
+	 */
+	private static String getTablenameByIndex(String currentLine) {
+		
+		 for (int i = 0; i < currentLine.length(); i++) {
+
+			 char currentCaracter = currentLine.charAt(i);
+		        // Si l'on tombe apres le CREATE TABLE et l'espace
+		    if (i >= createTableStr.length()) {
+
+		        // ... ET si l'on ne tombe pas sur la parenthese ouvrante
+		        if (!(Character.compare(currentCaracter, parenthese) == 0)) {
+		                 tableResult += Character.toString(currentCaracter);
+		        }
+		     }
+		 }
+		 
+		return tableResult;
+	}
+	
+	/**
+	 * getTablenameByRegex => get the table name(s) of generated .SQL or text file - Search by regular expression (regex)
+	 * @param currentLine
+	 * @return {String} The tablename
+	 */
+	private static String getTablenameByRegex(String currentLine) {
+		
+		Pattern createTablePattern = Pattern.compile(createTableStr, Pattern.CASE_INSENSITIVE);
+        Matcher create_table_matcher = createTablePattern.matcher(currentLine);
+         
+        if (create_table_matcher.find()) {
+        	tableResult = currentLine.replaceAll(createTableStr, "").replace(Character.toString(parenthese), "").trim();
+        	out.println(tableResult);
+        }
+		
+        return tableResult;
+	}	
 
     public static void main(String args[]) throws FileNotFoundException {
  
         File jMeriseSQL = new File("./test.sql");
         Scanner scanner = new Scanner(jMeriseSQL);
-        
-        int lineNumber = 1;
-        char parenthese = '(';
-        
-        String createTableStr = "CREATE TABLE ";
-        String tableResult;
+             
+        String tableResult = null;
         String[] list_of_tables = {};
-        
-        Pattern createTablePattern = Pattern.compile(createTableStr, Pattern.CASE_INSENSITIVE);
-        
+       
         while(scanner.hasNextLine()) {
         	
             String strCurrentLine = scanner.nextLine();
             int currenLineLength = strCurrentLine.length();
-            lineNumber++;
-        
-                      
+
             if (strCurrentLine.startsWith(createTableStr)) {
              
-                 tableResult = "";
-
-	    		 for (int i = 0; i < currenLineLength; i++) {
-	
-                     // ------------ INDEX VERSION ----------- //
-	    			 char currentCaracter = strCurrentLine.charAt(i);
-	    		        // Si l'on tombe apres le CREATE TABLE et l'espace
-	    		    if (i >= createTableStr.length()) {
-	
-	    		        // ... ET si l'on ne tombe pas sur la parenthese ouvrante
-	    		        if (!(Character.compare(currentCaracter,parenthese) == 0)) {
-	    		                 tableResult += Character.toString(currentCaracter);
-	    		        }
-	    		     }
-	    		 }
-	    		 
-	             // -------- REGEX VERSION ------------ // 
-	             Matcher create_table_matcher = createTablePattern.matcher(strCurrentLine);
-	             
-                if (create_table_matcher.find()) {
-                	tableResult = strCurrentLine.replaceAll(createTableStr, "").replace(Character.toString(parenthese), "").trim();
-                	out.println(tableResult);
-                }
-                
-               System.out.println(tableResult); 
+                 tableResult = getTablenameByRegex(strCurrentLine);
+                 
             } else {
             	strCurrentLine = "";
             }
-        }			
+        }
+        System.out.println(tableResult); 
     }      
  }  
